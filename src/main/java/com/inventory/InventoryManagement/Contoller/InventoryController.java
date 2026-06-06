7aa61a71d9478dabfa283744675f1220faea061b
Sriraj143 <drrkumar88@gmail.com>
2026-06-06 13:34:33 +0530

    modified controller

--- src/main/java/com/inventory/InventoryManagement/Contoller/InventoryController.java
+++ src/main/java/com/inventory/InventoryManagement/Contoller/InventoryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,7 @@ public class InventoryController {
 	@Autowired
     private InventoryService service;
 
-    @PostMapping("/search")
+    @GetMapping("/search")
     public ResponseEntity<Page<Inventory>> searchInventory(
 
             @RequestBody(required = false)
